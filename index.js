8f8a139a7c6d065107a765c58586894f5bd3bac5
ji sun lee <ellijs@hotmail.com>
2021-03-24 02:40:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function takeANumber(array, person) {
   array.push(person);
   return `Welcome, ${person}. You are number ${i} in line.`;
   }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.slice(1) === null) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeliLine.slice(1)
+  }
+}
