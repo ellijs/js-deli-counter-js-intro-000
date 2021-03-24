f165060d418f88626b8d36a434a9ed67ca81eb62
ji sun lee <ellijs@hotmail.com>
2021-03-24 02:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 function takeANumber(array, person) {
-  for ( let i = 1; i < array.length + 1; i++) {
-    array.push(`Welcome, ${person}. You are number ${i} in line.`);
-    }
-    return array;
+  let i = array.length + 1;
+  array.push(person);
+  return `Welcome, ${person}. You are number ${i} in line.`);
   }
